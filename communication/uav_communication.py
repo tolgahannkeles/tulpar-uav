1def5cb8e6f833ce2bb1c9b8d858fe182a9b6a33
Tolgahan Keleş <108351498+tolgahannkeles@users.noreply.github.com>
2025-01-20 16:54:37 +0300

    sending processes are separated with threads

--- communication/uav_communication.py
+++ communication/uav_communication.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 from utils.telemetry import TelemetryUtil
 from utils.udp_client import UDPClient
 from utils.camera import Camera
@@ -13,20 +16,31 @@ class UAVCommunication:
         self.telemetry_client = UDPClient(self.ip, self.ports["telemetry_port"])
         self.camera = Camera()
 
+
     def __send_video(self):
-        frame = self.camera.get_frame()
-        encoded_frame= FrameDecoder.encode(frame)
-        self.video_client.send(encoded_frame)
-
-    def __send_telemetry(self):
-        telemetry = TelemetryUtil.get_telemetry()
-        self.telemetry_client.send(telemetry)
-
-    def start_communication(self):
+        print("Sending Video")
         while True:
             try:
-                self.__send_video()
-                self.__send_telemetry()
+                frame = self.camera.get_frame()
+                encoded_frame = FrameDecoder.encode(frame)
+                self.video_client.send(encoded_frame)
             except Exception as e:
-                print(f"Error: {e}")
-                break
+                print(f"Video Error: {e}")
+
+    def __send_telemetry(self):
+        print("Sending Telemetry")
+        while True:
+            try:
+                telemetry = TelemetryUtil.get_telemetry()
+                self.telemetry_client.send(telemetry)
+                time.sleep(.2)
+            except Exception as e:
+                print(f"Telemetry Error: {e}")
+
+    def start_communication(self):
+        video_thread = threading.Thread(target=self.__send_video)
+        telemetry_thread = threading.Thread(target=self.__send_telemetry)
+        video_thread.start()
+        telemetry_thread.start()
+        video_thread.join()
+        telemetry_thread.join()
