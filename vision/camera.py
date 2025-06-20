dffa54d1daa1902d1d06277ff6e870eba73a46cb
Tolgahan Keleş <108351498+tolgahannkeles@users.noreply.github.com>
2025-06-20 20:19:55 +0300

    format

--- vision/camera.py
+++ vision/camera.py
@@ -6,7 +6,7 @@ class Camera:
     HEIGHT = 480
     WIDTH = 640
     def __init__(self):
-        self.camera = cv2.VideoCapture("./video/test-3.mp4")
+        self.camera = cv2.VideoCapture("./video/video3.mp4")
         self.camera.set(3, self.WIDTH)
         self.camera.set(4, self.HEIGHT)
 
